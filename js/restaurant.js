e30c1657314bd41d71d4c5425052b57e25e62c65
chawchitsuthwe <chawchitsu.thwe@gmail.com>
2022-12-19 13:32:09 +0900

    added restaurant page and code refactor

---
+++ js/restaurant.js
@@ -0,0 +1,13 @@
+var colorOne = "#f3f3f3",
+  colorTwo = "#CAAFA4";
+
+$(window).on("scroll touchmove", function () {
+  if ($(document).scrollTop() >= $("#first-section").position().top) {
+    $('.main-content').css('background-color', colorOne);
+    $('.main-content').css('transition', "all 0.5s ease-out");
+  };
+  if ($(document).scrollTop() > $("#second-section").position().top) {
+    $('.main-content').css('background-color', colorTwo);
+    $('.main-content').css('transition', "all 0.5s ease-out");
+  };
+});
